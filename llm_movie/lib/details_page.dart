b78b3a0f244fd97e017a4127fb752a5845f50957
HitchingHobo <100282040+HitchingHobo@users.noreply.github.com>
2024-01-02 10:36:04 +0100

    Create details_page.dart

---
+++ llm_movie/lib/details_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:llm_movie/utilities/movie_provider.dart';
+import 'package:provider/provider.dart';
+
+class DetailsPage extends StatelessWidget {
+  DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
